0a44d6acc02b34c50517dbdd24186f5ee77d19cb
Simon <slkmailbox@gmail.com>
2022-04-24 01:36:07 +0100

    furthur work on the front page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import Intro from './components/intro';
 import ProductList from './components/ProductList';
 import Contact from './components/Contact';
+import Sect1 from './components/sect1';
 
 function App() {
   return (
     <div className="App">
-      <Intro />
+      <Sect1 />
       <ProductList />
       <Contact />
     </div>
---
+++ src/components/sect1.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './sect1.css';
+import MyLogo from '../images/logo.png';
+import { HiMenu } from "react-icons/hi";
+import { FaHourglassHalf } from "react-icons/fa";
+import { FaHatCowboy } from "react-icons/fa";
+import { MdOutlineAlternateEmail } from "react-icons/md";
+
+const sect1 = () => {
+  return (
+    <div className='sect1-container'>
+        <div className='top-bar-wrapper'>
+            <div className="top-bar">
+                <div className="top-left">
+                    <img src={MyLogo} alt="logo" />
+                </div>
+                <div className="top-right">
+                    <div className="menu-wrapper">
+                        <div className="menu-btn"><HiMenu /></div>
+                        <ul className="menu-items">
+                            <li><a href="#" className='link-item'><FaHatCowboy /> <span className='text-gap'>Skills</span></a></li>
+                            <li><a href="#" className='link-item'><FaHourglassHalf /> <span className='text-gap'>Recent Work</span></a></li>
+                            <li><a href="#" className='link-item'><MdOutlineAlternateEmail /> <span className='text-gap'>Contact</span></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="page-heading">
+            <h1>Front-end Developer &amp; Designer</h1>
+            
+        </div>
+    </div>
+  )
+}
+
+export default sect1
